51be3c93702a87ebbee205703add36bb84634580
Ratnesh <ratnesh.navlakhe@go-jek.com>
2018-06-17 19:16:57 +0530

    Fetch the data from an api use okhttp, Gson

--- app/src/main/java/com/example/ratneshnavlakhe/kotlinyoutubeapp/MainActivity.kt
+++ app/src/main/java/com/example/ratneshnavlakhe/kotlinyoutubeapp/MainActivity.kt
@@ -4,7 +4,10 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +17,44 @@ class MainActivity : AppCompatActivity() {
 
 //        recyclerView_main.setBackgroundColor(Color.BLUE)
         recyclerView_main.layoutManager = LinearLayoutManager(this)
-        recyclerView_main.adapter = MainAdapter()
+//        recyclerView_main.adapter = MainAdapter()
+
+        fetchJson()
+    }
+
+    fun fetchJson() {
+        println("Attempting to Fetch Json")
+
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute request")
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+
+                runOnUiThread {
+                    recyclerView_main.adapter = MainAdapter(homeFeed)
+                }
+            }
+        })
     }
 }
+
+class HomeFeed(val videos: List<Video>)
+
+class Video(val id: Int, val name: String, val link: String, val imageUrl: String,
+            val numberOfViews: Int, val channel: Channel)
+
+class Channel(val name: String, val profileImageUrl: String)
--- app/src/main/java/com/example/ratneshnavlakhe/kotlinyoutubeapp/MainAdapter.kt
+++ app/src/main/java/com/example/ratneshnavlakhe/kotlinyoutubeapp/MainAdapter.kt
@@ -6,12 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.video_row.view.*
 
-class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
 
     val videoTitles = listOf<String>("First Title", "Second", "3rd")
     //number of items
     override fun getItemCount(): Int {
-        return videoTitles.size
+        return homeFeed.videos.count()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
@@ -22,8 +22,10 @@ class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        val videoTitle = videoTitles.get(position)
-        holder.view?.textView_video_title.text = videoTitle
+//        val videoTitle = videoTitles.get(position)
+        val video = homeFeed.videos.get(position)
+        holder.view?.textView_video_title.text = video.name
+
     }
 }
 
